6016dd49158a24a4a07d5ed0f50ec5adf91d3bee
suaddedushi1 <sd35457@ubt-uni.net>
2018-01-25 16:07:56 +0100

    setup mongodb, views,

--- app.js
+++ app.js
@@ -5,13 +5,42 @@ const express = require('express');
 // init app
 const app = express();
 
-//routes
-app.get('/', function(req, res) {
-  res.send("Index page");
+// Views location
+app.set('views', __dirname + '/views');
+
+// Setting template engine
+app.set('view engine', 'ejs');
+
+// Serving static files
+app.use(express.static(__dirname + '/public'));
+
+// body parser middleware
+var bodyParser = require('body-parser');
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Setup MongoDB
+const MongoClient = require('mongodb').MongoClient;
+const mongoURL = 'mongodb://localhost:27017/techsperience';
+const ObjectId = require('mongodb').ObjectId;
+
+// Conneting to MongoDB
+MongoClient.connect(mongoURL, function(err, db) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Database connected successfully!');
+    dokumentet = db.collection('dokumentet');
+  }
 });
 
+//routes
+app.get('/', function(req, res) {
+  res.render('index');
+});
 
-
-app.listen(3000, function() {
-  console.log("App running at http://localhost:3000");
+app.listen(3005, function() {
+  console.log("App running at http://localhost:3005");
 });
---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+console.log('ktu ka js');
